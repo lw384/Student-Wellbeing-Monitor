b5bbf92b12052cce5282390647178dcbcae5bc30
lw384 <15651756926@163.com>
2025-11-29 23:44:01 +0000

    feat: add a start script can mock and start serve at the same time

--- src/student_wellbeing_monitor/tools/test.py
+++
@@ -1,9 +0,0 @@
-from student_wellbeing_monitor.database.db_core import get_conn
-
-conn = get_conn()
-cur = conn.cursor()
-cur.execute("SELECT COUNT(*) FROM student")
-print("rows in student:", cur.fetchone()[0])
-cur.execute("SELECT * FROM student LIMIT 5")
-print(cur.fetchall())
-conn.close()
