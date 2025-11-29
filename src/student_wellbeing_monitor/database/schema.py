ee47e24c299f9cd1f1cef9d911e5d87e8d541c25
lw384 <15651756926@163.com>
2025-11-29 15:47:53 +0000

    refactor: data schema change

--- src/student_wellbeing_monitor/database/schema.py
+++ src/student_wellbeing_monitor/database/schema.py
@@ -1,4 +1,5 @@
 # schema.py
+# create database schema
 from student_wellbeing_monitor.database.db_core import get_conn
 
 
@@ -8,19 +9,6 @@ def init_db_schema():
     cur = conn.cursor()
 
     # --------------------
-    # students
-    # --------------------
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS students (
-            student_id TEXT PRIMARY KEY,
-            name       TEXT NOT NULL,
-            email      TEXT
-        )
-    """
-    )
-
-    # --------------------
     # programme
     # --------------------
     cur.execute(
@@ -28,20 +16,53 @@ def init_db_schema():
         CREATE TABLE IF NOT EXISTS programme (
             programme_id     TEXT PRIMARY KEY,
             programme_name   TEXT NOT NULL,
-            programme_code   TEXT
+            programme_code   TEXT UNIQUE
         )
     """
     )
 
     # --------------------
-    # modules
+    # student
     # --------------------
     cur.execute(
         """
-        CREATE TABLE IF NOT EXISTS modules (
-            module_id    INTEGER
-            module_code  TEXT PRIMARY KEY,
-            module_name  TEXT NOT NULL
+        CREATE TABLE IF NOT EXISTS student (
+            student_id TEXT PRIMARY KEY,
+            name       TEXT NOT NULL,
+            email      TEXT,
+            programme_id TEXT NOT NULL,
+            FOREIGN KEY (programme_id) REFERENCES programme(programme_id)
+        )
+    """
+    )
+
+    # --------------------
+    # module
+    # --------------------
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS module (
+            module_id    TEXT PRIMARY KEY,
+            module_code  TEXT UNIQUE,
+            module_name  TEXT NOT NULL,
+            programme_id TEXT NOT NULL,
+            FOREIGN KEY (programme_id) REFERENCES programme(programme_id)
+        )
+    """
+    )
+
+    # --------------------
+    # student_module (relation table)
+    # --------------------
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS student_module (
+            id          INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_id  TEXT NOT NULL,
+            module_id   TEXT NOT NULL,
+            FOREIGN KEY(student_id) REFERENCES student(student_id),
+            FOREIGN KEY(module_id) REFERENCES module(module_id),
+            UNIQUE(student_id, module_id)
         )
     """
     )
@@ -53,76 +74,66 @@ def init_db_schema():
         """
         CREATE TABLE IF NOT EXISTS wellbeing (
             id            INTEGER PRIMARY KEY AUTOINCREMENT,
-            student_id    INTEGER NOT NULL,
+            student_id    TEXT NOT NULL,
             week          INTEGER NOT NULL,
-            stress_level  INTEGER,
-            hours_slept   INTEGER,
+            stress_level  INTEGER CHECK(stress_level BETWEEN 1 AND 5),
+            hours_slept   REAL CHECK(hours_slept BETWEEN 0 AND 12),
             comment       TEXT,
-            FOREIGN KEY(student_id) REFERENCES students(student_id)
+            FOREIGN KEY(student_id) REFERENCES student(student_id),
+            UNIQUE(student_id, week)
+        );
+        """
+    )
+
+    # --------------------
+    # attendance
+    # --------------------
+    cur.execute(
+        """
+       CREATE TABLE IF NOT EXISTS attendance (
+            id              INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_id      TEXT NOT NULL,
+            module_id       TEXT NOT NULL,
+            week            INTEGER NOT NULL,
+            session_number  INTEGER NOT NULL DEFAULT 1,   -- default 1 lesson every week
+            status          INTEGER NOT NULL CHECK(status IN (0, 1)),   -- 0 absent; 1 present
+            FOREIGN KEY (student_id) REFERENCES student(student_id),
+            FOREIGN KEY (module_id) REFERENCES module(module_id),
+            UNIQUE(student_id, module_id, week, session_number)
+        );
+        """
+    )
+
+    # --------------------
+    # submission
+    # --------------------
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS submission (
+            id           INTEGER PRIMARY KEY AUTOINCREMENT,
+            student_id   TEXT NOT NULL,
+            module_id    TEXT NOT NULL,
+            submitted    INTEGER NOT NULL CHECK(submitted IN (0, 1)),   -- 1 submitted; 0 no
+            grade        REAL CHECK(grade BETWEEN 0 AND 100), 
+            assignment_no  INTEGER NOT NULL DEFAULT 1, 
+            FOREIGN KEY(student_id) REFERENCES student(student_id)
+            FOREIGN KEY(module_id)  REFERENCES module(module_id),  
+            UNIQUE(student_id, module_id, assignment_no)       
         )
     """
     )
 
-    # # --------------------
-    # # attendance
-    # # --------------------
-    # cur.execute(
-    #     """
-    #     CREATE TABLE IF NOT EXISTS attendance (
-    #         id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         student_id  INTEGER NOT NULL,
-    #         module_code TEXT NOT NULL,
-    #         week        INTEGER NOT NULL,
-    #         status      TEXT NOT NULL,
-    #         FOREIGN KEY(student_id) REFERENCES students(student_id)
-    #     )
-    # """
-    # )
-
-    # # --------------------
-    # # submissions
-    # # --------------------
-    # cur.execute(
-    #     """
-    #     CREATE TABLE IF NOT EXISTS submissions (
-    #         id           INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         student_id   INTEGER NOT NULL,
-    #         module_code  TEXT NOT NULL,
-    #         submitted    INTEGER NOT NULL,   -- 0/1
-    #         grade        REAL,               -- allow NULL
-    #         FOREIGN KEY(student_id) REFERENCES students(student_id)
-    #     )
-    # """
-    # )
-
-    # # --------------------
-    # # student_modules (relation table)
-    # # --------------------
-    # cur.execute(
-    #     """
-    #     CREATE TABLE IF NOT EXISTS student_modules (
-    #         id          INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         student_id  INTEGER NOT NULL,
-    #         module_code TEXT NOT NULL,
-    #         FOREIGN KEY(student_id) REFERENCES students(student_id),
-    #         FOREIGN KEY(module_code) REFERENCES modules(module_code)
-    #     )
-    # """
-    # )
-
-    # # --------------------
-    # # user accounts
-    # # --------------------
-    # cur.execute(
-    #     """
-    #     CREATE TABLE IF NOT EXISTS users (
-    #         id            INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         username      TEXT UNIQUE NOT NULL,
-    #         password_hash TEXT NOT NULL,
-    #         role          TEXT NOT NULL
-    #     )
-    # """
-    # )
+    # --------------------
+    # user accounts
+    # --------------------
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS user (
+            id            INTEGER PRIMARY KEY AUTOINCREMENT,
+            role          TEXT NOT NULL
+        )
+    """
+    )
 
     conn.commit()
     conn.close()
